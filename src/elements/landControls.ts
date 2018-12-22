24896d37ee686c1cc6e2dc5b317a7ff3684bb66d
Andrew Brower <andrewbrower@captain401.com>
2018-12-22 15:15:13 -0800

    add buttons

--- src/elements/landControls.ts
+++ src/elements/landControls.ts
@@ -1,13 +1,34 @@
+import { filter } from 'lodash';
 import { loadTemplate } from '.';
+import { info } from '../logging';
 
 export async function getLandControlsElement() {
   const template = await loadTemplate('landControls');
+
   return class LandControlsElement extends HTMLElement {
-    protected root: ShadowRoot;
+    private buttonMap: { [key: string]: Element } = {};
+    private selected: string | undefined;
     constructor() {
       super();
-      this.root = this.attachShadow({ mode: 'open' });
-      this.root.appendChild(template.content.cloneNode(true));
+      this.appendChild(template.content.cloneNode(true));
+      this.getButtons();
+    }
+    public onButtonPress(id: string) {
+      this.selected = id;
+      info('button pressed', { id });
+      for (const button of Object.values(this.buttonMap)) {
+        button.classList.remove('pressed');
+      }
+      this.buttonMap[id].classList.add('pressed');
+    }
+    private getButtons() {
+      const buttons: NodeListOf<HTMLSpanElement> = document.querySelectorAll(
+        'land-controls .ui-button',
+      );
+      for (const button of buttons) {
+        this.buttonMap[button.id] = button;
+        button.onmouseup = this.onButtonPress.bind(this, button.id);
+      }
     }
   };
 }
