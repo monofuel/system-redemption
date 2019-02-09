931ce9deea5c52aebb62e063602ed11bd8960f78
Andrew Brower <andrewbrower@captain401.com>
2019-02-09 14:07:29 -0800

    setup movement blocking

--- src/events/serverContext.ts
+++ src/events/serverContext.ts
@@ -1,202 +1,211 @@
-import { ServerEvent, ServerEventKinds, ServerEvents, GameStage, FrontendEvent } from ".";
+import {
+  ServerEvent,
+  ServerEventKinds,
+  ServerEvents,
+  GameStage,
+  FrontendEvent
+} from ".";
 import { GameState, newGameState, applyEvent } from "./state";
 import { EventQueue } from "./queues";
 import { info } from "../logging";
-import { pathfind } from "../services/pathfind";
+import { pathfind, getTileInDirection } from "../services/pathfind";
 import { deflateSync } from "zlib";
 import { delay } from "../util";
 
 interface LoggedEvent {
-    event: ServerEvent;
-    timestamp: number;
-    listeners: number;
+  event: ServerEvent;
+  timestamp: number;
+  listeners: number;
 }
 
 export class UpdateLoop {
-    private name: string;
-    private fn: (delta: number) => boolean;
-    private freq: number;
-    private stopFlag: boolean = false;
+  private name: string;
+  private fn: (delta: number) => boolean;
+  private freq: number;
+  private stopFlag: boolean = false;
 
-    constructor(name: string, fn: (delta: number) => boolean, freq: number) {
-        this.name = name;
-        this.fn = fn;
-        this.freq = freq;
-    }
+  constructor(name: string, fn: (delta: number) => boolean, freq: number) {
+    this.name = name;
+    this.fn = fn;
+    this.freq = freq;
+  }
 
-    public start() {
-        let lastTime = Date.now();
-        const loopFn = () => {
-            if (this.stopFlag) {
-                info('detaching loop', { name: this.name });
-                return;
-            }
-            const startTime = Date.now();
-            const end = this.fn(startTime - lastTime);
-            if (end) {
-                info('detaching loop', { name: this.name });
-                return;
-            }
-            lastTime = Date.now();
-            setTimeout(loopFn, 1000 / this.freq - (lastTime - startTime));
-        };
-        loopFn();
-    }
+  public start() {
+    let lastTime = Date.now();
+    const loopFn = () => {
+      if (this.stopFlag) {
+        info("detaching loop", { name: this.name });
+        return;
+      }
+      const startTime = Date.now();
+      const end = this.fn(startTime - lastTime);
+      if (end) {
+        info("detaching loop", { name: this.name });
+        return;
+      }
+      lastTime = Date.now();
+      setTimeout(loopFn, 1000 / this.freq - (lastTime - startTime));
+    };
+    loopFn();
+  }
 
-    public stop() {
-        this.stopFlag = true;
-    }
+  public stop() {
+    this.stopFlag = true;
+  }
 }
-
 
 // TODO this class is not tested
 // similar to PlayElement and EventContextElement
 export class ServerContext {
-    public queue: EventQueue<ServerEventKinds, ServerEvents>;
+  public queue: EventQueue<ServerEventKinds, ServerEvents>;
 
+  public events: LoggedEvent[] = [];
+  public gameState: GameState;
+  private asyncEvents: ServerEvent[] = [];
 
-    public events: LoggedEvent[] = [];
-    public gameState: GameState;
-    private asyncEvents: ServerEvent[] = [];
+  private gameTickLoop?: UpdateLoop;
+  private flushLoop: UpdateLoop;
 
-    private gameTickLoop?: UpdateLoop;
-    private flushLoop: UpdateLoop;
+  public onGameEvent?: (event: ServerEvent | FrontendEvent) => void;
 
-    public onGameEvent?: (event: ServerEvent | FrontendEvent) => void;
-
-    constructor() {
-        this.gameState = newGameState();
-        this.queue = new EventQueue({
-            postSyncronous: false,
-            preHandler: (event) => {
-                applyEvent(this.gameState, event);
-            },
-            logger: (event, timestamp, listeners) => {
-                this.events.push({ event, timestamp, listeners });
-                info('event posted', {
-                    event: JSON.stringify(event),
-                    timestamp,
-                    listeners,
-                });
-                if (this.onGameEvent) {
-                    this.onGameEvent(event);
-                }
-            },
+  constructor() {
+    this.gameState = newGameState();
+    this.queue = new EventQueue({
+      postSyncronous: false,
+      preHandler: event => {
+        applyEvent(this.gameState, event);
+      },
+      logger: (event, timestamp, listeners) => {
+        this.events.push({ event, timestamp, listeners });
+        info("event posted", {
+          event: JSON.stringify(event),
+          timestamp,
+          listeners
         });
-
-        this.queue.addListener('gameTick', () => {
-            if (this.gameState.stage.mode === GameStage.running) {
-
-                const events = this.onTick();
-                info('handling ontick events', { length: events.length });
-                for (const e of events) {
-                    this.queue.post(e);
-                }
-            }
-        });
-        this.queue.addListener('newFiniteMap', (e) => {
-            const { tps } = e.map;
-            this.gameTickLoop = new UpdateLoop('gameTick', (delta) => {
-                if (this.gameState.stage.mode === GameStage.running) {
-                    info('game tick', { delta });
-
-                    this.queue.post({
-                        kind: 'gameTick'
-                    })
-                    this.queue.flushAll();
-                }
-                return this.gameState.stage.mode === GameStage.done;
-            }, tps);
-            this.gameTickLoop.start();
-        });
-
-        this.flushLoop = new UpdateLoop(
-            'queueFlush',
-            (delta: number) => {
-                this.queue.flushAll();
-                return false;
-            },
-            30,
-        );
-        this.flushLoop.start();
-    }
-
-    public dispose() {
-        if (this.gameTickLoop) {
-            this.gameTickLoop.stop();
+        if (this.onGameEvent) {
+          this.onGameEvent(event);
         }
-    }
-    public async loadLog(events: Array<ServerEvent>) {
-        this.gameState = newGameState();
-        this.events = [];
+      }
+    });
 
-        for (const event of events) {
-            if (event.kind === 'assertion') {
-                continue;
-            }
-            this.queue.post(event);
+    this.queue.addListener("gameTick", () => {
+      if (this.gameState.stage.mode === GameStage.running) {
+        const events = this.onTick();
+        info("handling ontick events", { length: events.length });
+        for (const e of events) {
+          this.queue.post(e);
         }
+      }
+    });
+    this.queue.addListener("newFiniteMap", e => {
+      const { tps } = e.map;
+      this.gameTickLoop = new UpdateLoop(
+        "gameTick",
+        delta => {
+          if (this.gameState.stage.mode === GameStage.running) {
+            info("game tick", { delta });
+
+            this.queue.post({
+              kind: "gameTick"
+            });
+            this.queue.flushAll();
+          }
+          return this.gameState.stage.mode === GameStage.done;
+        },
+        tps
+      );
+      this.gameTickLoop.start();
+    });
+
+    this.flushLoop = new UpdateLoop(
+      "queueFlush",
+      (delta: number) => {
+        this.queue.flushAll();
+        return false;
+      },
+      30
+    );
+    this.flushLoop.start();
+  }
+
+  public dispose() {
+    if (this.gameTickLoop) {
+      this.gameTickLoop.stop();
+    }
+  }
+  public async loadLog(events: Array<ServerEvent>) {
+    this.gameState = newGameState();
+    this.events = [];
+
+    for (const event of events) {
+      if (event.kind === "assertion") {
+        continue;
+      }
+      this.queue.post(event);
+    }
+  }
+
+  // onTick should run after every game tick
+  // it should not be called when replaying the event log
+  // it does game tick work for appending new events
+  onTick(): ServerEvent[] {
+    const results: ServerEvent[] = [];
+    onTick(this.gameState, this.asyncEvents);
+
+    while (this.asyncEvents.length > 0) {
+      results.push(this.asyncEvents.shift()!);
     }
 
-    // onTick should run after every game tick
-    // it should not be called when replaying the event log
-    // it does game tick work for appending new events
-    onTick(): ServerEvent[] {
-        const results: ServerEvent[] = [];
-        onTick(this.gameState, this.asyncEvents);
+    return results;
+  }
 
-        while (this.asyncEvents.length > 0) {
-            results.push(this.asyncEvents.shift()!);
-        }
-
-        return results;
-    }
-
-    addAsyncEvent(event: ServerEvent) {
-        this.asyncEvents.push(event);
-    }
+  addAsyncEvent(event: ServerEvent) {
+    this.asyncEvents.push(event);
+  }
 }
 
 /**
- * 
+ *
  * @param state READ ONLY game state (may change if read asyncronously)
  * @param asyncEvents array to push to for events (can be done asyncronously)
  */
 export function onTick(state: GameState, asyncEvents: ServerEvent[]) {
-
-    for (const uuid in state.units) {
-        const unit = state.units[uuid];
-        const def = state.unitDefinitions[unit.type]!;
-        if (unit.destination && !unit.path) {
-            const path = pathfind(state, uuid, unit.destination);
-            if (path.length === 0) {
-                // no path found, clear destination
-                asyncEvents.push({
-                    kind: 'setDestination',
-                    uuids: [uuid]
-                })
-            } else {
-
-                asyncEvents.push({
-                    kind: 'setPath',
-                    uuid,
-                    dest: unit.destination,
-                    path,
-                });
-            }
-        }
-        if (unit.moveCooldown === 0 && unit.path && unit.path.length > 0) {
-            asyncEvents.push({
-                kind: 'moveUnit',
-                uuid,
-                dir: unit.path[0]
-            })
-        }
-        if (unit.health && def.maxHealth !== 0 && unit.health <= 0) {
-            asyncEvents.push({
-                kind: 'destroyUnit',
-                uuid
-            })
-        }
+  for (const uuid in state.units) {
+    const unit = state.units[uuid];
+    const def = state.unitDefinitions[unit.type]!;
+    if (unit.destination && !unit.path) {
+      const path = pathfind(state, uuid, unit.destination);
+      if (path.length === 0) {
+        // no path found, clear destination
+        asyncEvents.push({
+          kind: "setDestination",
+          uuids: [uuid]
+        });
+      } else {
+        asyncEvents.push({
+          kind: "setPath",
+          uuid,
+          dest: unit.destination,
+          path
+        });
+      }
     }
+    if (unit.moveCooldown === 0 && unit.path && unit.path.length > 0) {
+      const dir = unit.path[0];
+      const nextLoc = getTileInDirection(unit.loc, dir);
+      if (!state.cache.unitLocations[nextLoc]) {
+        asyncEvents.push({
+          kind: "moveUnit",
+          uuid,
+          dir
+        });
+      }
+    }
+    if (unit.health && def.maxHealth !== 0 && unit.health <= 0) {
+      asyncEvents.push({
+        kind: "destroyUnit",
+        uuid
+      });
+    }
+  }
 }
--- src/test/eventLogs/unitBlocking.ts
+++ src/test/eventLogs/unitBlocking.ts
@@ -1,231 +1,174 @@
 import { FrontendEvent, ServerEvent } from "../../events";
 
 export const unitBlockingTestLog: (ServerEvent | FrontendEvent)[] = [
-    {
-        "kind": "newFiniteMap",
-        "map": {
-            "name": "foobar",
-            "version": 1,
-            "tps": 2,
-            "landColor": 4215094,
-            "edgeColor": 7311936,
-            "cliffColor": 3550758,
-            "waterColor": 5484770,
-            "sunColor": 13421772,
-            "zScale": 0.2,
-            "size": 1,
-            "chunkSize": 3,
-            "waterHeight": 1.8,
-            "grid": [
-                [
-                    {
-                        "x": 0,
-                        "y": 0,
-                        "grid": [
-                            [
-                                [
-                                    1,
-                                    1,
-                                    1,
-                                    1
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ]
-                            ],
-                            [
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ]
-                            ],
-                            [
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ],
-                                [
-                                    2,
-                                    2,
-                                    2,
-                                    2
-                                ]
-                            ]
-                        ]
-                    }
-                ]
+  {
+    kind: "newFiniteMap",
+    map: {
+      name: "foobar",
+      version: 1,
+      tps: 2,
+      landColor: 4215094,
+      edgeColor: 7311936,
+      cliffColor: 3550758,
+      waterColor: 5484770,
+      sunColor: 13421772,
+      zScale: 0.2,
+      size: 1,
+      chunkSize: 3,
+      waterHeight: 1.8,
+      grid: [
+        [
+          {
+            x: 0,
+            y: 0,
+            grid: [
+              [[1, 1, 1, 1], [2, 2, 2, 2], [2, 2, 2, 2]],
+              [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]],
+              [[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]
             ]
-        }
-    },
-    {
-        "kind": "defineUnit",
-        "unit": {
-            "type": "tank",
-            "size": 1,
-            "buildTime": 10,
-            "cost": 100,
-            "maxHealth": 200,
-            "layer": "ground",
-            "graphical": {
-                "model": "LightTankLvl1"
-            },
-            "move": {
-                "cooldown": 1
-            }
-        }
-    },
-    {
-        "kind": "defineUnit",
-        "unit": {
-            "type": "ltank2",
-            "size": 1,
-            "buildTime": 10,
-            "cost": 100,
-            "maxHealth": 200,
-            "layer": "ground",
-            "graphical": {
-                "model": "LightTankLvl2"
-            },
-            "move": {
-                "cooldown": 2
-            }
-        }
-    },
-    {
-        "kind": "defineUnit",
-        "unit": {
-            "type": "ltank3",
-            "size": 1,
-            "buildTime": 10,
-            "cost": 100,
-            "maxHealth": 200,
-            "layer": "ground",
-            "graphical": {
-                "model": "LightTankLvl3"
-            },
-            "move": {
-                "cooldown": 3
-            }
-        }
-    },
-    {
-        "kind": "defineUnit",
-        "unit": {
-            "type": "htank3",
-            "size": 1,
-            "buildTime": 10,
-            "cost": 100,
-            "maxHealth": 200,
-            "layer": "ground",
-            "graphical": {
-                "model": "HeavyTankLvl3"
-            },
-            "move": {
-                "cooldown": 3
-            }
-        }
-    },
-    {
-        "kind": "editorMode",
-        "selection": "newUnit",
-        "user": "blue",
-        "unitType": "tank"
-    },
-    {
-        "kind": "newUnit",
-        "unit": {
-            "uuid": "0caa58ac-bc75-4c09-a58a-861ec8e3a8af",
-            "type": "tank",
-            "facing": "E",
-            "size": 1,
-            "loc": "0:1",
-            "color": "blue",
-            "map": "test",
-            "moveCooldown": 0
-        }
-    },
-    {
-        "kind": "newUnit",
-        "unit": {
-            "uuid": "68d28c38-867f-4fda-90a9-5d810b20887e",
-            "type": "tank",
-            "facing": "E",
-            "size": 1,
-            "loc": "1:1",
-            "color": "blue",
-            "map": "test",
-            "moveCooldown": 0
-        }
-    },
-    {
-        "kind": "newUnit",
-        "unit": {
-            "uuid": "b4e87182-8711-448b-bb69-34125c924f97",
-            "type": "tank",
-            "facing": "E",
-            "size": 1,
-            "loc": "2:1",
-            "color": "blue",
-            "map": "test",
-            "moveCooldown": 0
-        }
-    },
-    {
-        "kind": "newUnit",
-        "unit": {
-            "uuid": "8f1d6919-ad36-47f9-ba5c-134e89506c7e",
-            "type": "tank",
-            "facing": "E",
-            "size": 1,
-            "loc": "1:2",
-            "color": "blue",
-            "map": "test",
-            "moveCooldown": 0
-        }
-    },
-    /* {
-         kind: 'assertFail',
-         event: {
-             kind: 'moveUnit',
-             uuid: "8f1d6919-ad36-47f9-ba5c-134e89506c7e",
-             dir: 'S',
-         }
-     }, */
-    {
-        "kind": "gameTick"
-    },
-    {
-        "kind": "gameTick"
-    },
+          }
+        ]
+      ]
+    }
+  },
+  {
+    kind: "defineUnit",
+    unit: {
+      type: "tank",
+      size: 1,
+      buildTime: 10,
+      cost: 100,
+      maxHealth: 200,
+      layer: "ground",
+      graphical: {
+        model: "LightTankLvl1"
+      },
+      move: {
+        cooldown: 1
+      }
+    }
+  },
+  {
+    kind: "defineUnit",
+    unit: {
+      type: "ltank2",
+      size: 1,
+      buildTime: 10,
+      cost: 100,
+      maxHealth: 200,
+      layer: "ground",
+      graphical: {
+        model: "LightTankLvl2"
+      },
+      move: {
+        cooldown: 2
+      }
+    }
+  },
+  {
+    kind: "defineUnit",
+    unit: {
+      type: "ltank3",
+      size: 1,
+      buildTime: 10,
+      cost: 100,
+      maxHealth: 200,
+      layer: "ground",
+      graphical: {
+        model: "LightTankLvl3"
+      },
+      move: {
+        cooldown: 3
+      }
+    }
+  },
+  {
+    kind: "defineUnit",
+    unit: {
+      type: "htank3",
+      size: 1,
+      buildTime: 10,
+      cost: 100,
+      maxHealth: 200,
+      layer: "ground",
+      graphical: {
+        model: "HeavyTankLvl3"
+      },
+      move: {
+        cooldown: 3
+      }
+    }
+  },
+  {
+    kind: "editorMode",
+    selection: "newUnit",
+    user: "blue",
+    unitType: "tank"
+  },
+  {
+    kind: "newUnit",
+    unit: {
+      uuid: "0caa58ac-bc75-4c09-a58a-861ec8e3a8af",
+      type: "tank",
+      facing: "E",
+      size: 1,
+      loc: "0:1",
+      color: "blue",
+      map: "test",
+      moveCooldown: 0
+    }
+  },
+  {
+    kind: "newUnit",
+    unit: {
+      uuid: "68d28c38-867f-4fda-90a9-5d810b20887e",
+      type: "tank",
+      facing: "E",
+      size: 1,
+      loc: "1:1",
+      color: "blue",
+      map: "test",
+      moveCooldown: 0
+    }
+  },
+  {
+    kind: "newUnit",
+    unit: {
+      uuid: "b4e87182-8711-448b-bb69-34125c924f97",
+      type: "tank",
+      facing: "E",
+      size: 1,
+      loc: "2:1",
+      color: "blue",
+      map: "test",
+      moveCooldown: 0
+    }
+  },
+  {
+    kind: "newUnit",
+    unit: {
+      uuid: "8f1d6919-ad36-47f9-ba5c-134e89506c7e",
+      type: "tank",
+      facing: "E",
+      size: 1,
+      loc: "1:2",
+      color: "blue",
+      map: "test",
+      moveCooldown: 0
+    }
+  },
+  {
+    kind: "assertFail",
+    event: {
+      kind: "moveUnit",
+      uuid: "8f1d6919-ad36-47f9-ba5c-134e89506c7e",
+      dir: "S"
+    }
+  },
+  {
+    kind: "gameTick"
+  },
+  {
+    kind: "gameTick"
+  }
 ] as any;
