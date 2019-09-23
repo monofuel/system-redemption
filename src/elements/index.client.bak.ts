86e18050b90c4f86bb2d2e817b8ef943c8d586f2
monofuel <monofuel@japura.net>
2019-09-23 03:00:56 +0000

    switch back to canvas

--- src/elements/index.client.bak.ts
+++
@@ -1,7 +0,0 @@
-import { assert } from 'chai';
-import { loadElements } from '.';
-describe('frontend test', () => {
-  it('test loading elements', () => {
-    loadElements();
-  });
-});
---
+++ src/elements/index.client.ts
@@ -0,0 +1,15 @@
+import { EventContextElement } from './eventContext';
+
+describe('frontend test', () => {
+  it('test loading elements', () => {
+    // loadElements();
+    //  window.customElements.define('event-context', EventContextElement);
+    //window.customElements.define('tile-test', TileTestElement);
+    // window.customElements.define('log-player', LogPlayerElement);
+    //window.customElements.define('three-scene', ThreeSceneElement);
+    //window.customElements.define('map-editor', MapEditorElement);
+    //window.customElements.define('log-tests', LogTestElement);
+    //window.customElements.define('model-view', ModelViewElement);
+    //window.customElements.define('play-sr', PlayELement);
+  });
+});
