3f2ec444a9ad28776f696275c7e5d706e0900b8d
monofuel <monofuel@japura.net>
2019-09-23 04:32:43 +0000

    remove check

--- src/services/hash.ts
+++ src/services/hash.ts
@@ -1,61 +1,58 @@
-import { LocHash } from "../types/SR";
+import { LocHash } from '../types/SR';
 
 interface HashMeta {
-    hash: LocHash,
-    x: number,
-    y: number
+  hash: LocHash;
+  x: number;
+  y: number;
 }
 
 const hashCache: {
-    [key: number]: {
-        [key: number]: HashMeta
-    }
+  [key: number]: {
+    [key: number]: HashMeta;
+  };
 } = {};
 
 const mapOfHashes: { [key: string]: HashMeta } = {};
 
 /**
  * Re-use hash strings to avoid garbage
- * @param x 
- * @param y 
+ * @param x
+ * @param y
  */
 export function getHash(x: number, y: number): LocHash {
-    if (!hashCache[x]) {
-        hashCache[x] = {};
-    }
-    if (!hashCache[x][y]) {
-        const meta: HashMeta = {
-            hash: `${x}:${y}`,
-            x,
-            y
-        }
-        hashCache[x][y] = meta;
-        mapOfHashes[meta.hash] = meta;
-    }
+  if (!hashCache[x]) {
+    hashCache[x] = {};
+  }
+  if (!hashCache[x][y]) {
+    const meta: HashMeta = {
+      hash: `${x}:${y}`,
+      x,
+      y,
+    };
+    hashCache[x][y] = meta;
+    mapOfHashes[meta.hash] = meta;
+  }
 
-    return hashCache[x][y].hash;
+  return hashCache[x][y].hash;
 }
 
 export function unHash(hash: string): [number, number] {
-    if (mapOfHashes[hash]) {
-        const { x, y } = mapOfHashes[hash];
-        return [x, y];
-    }
-    if (typeof hash !== 'string') {
-        debugger;
-    }
-    const split = hash.split(':');
-    const x = Number(split[0]);
-    const y = Number(split[1]);
-    const meta = {
-        hash,
-        x,
-        y
-    }
-    if (!hashCache[x]) {
-        hashCache[x] = {};
-    }
-    hashCache[x][y] = meta;
-    mapOfHashes[meta.hash] = meta;
+  if (mapOfHashes[hash]) {
+    const { x, y } = mapOfHashes[hash];
     return [x, y];
+  }
+  const split = hash.split(':');
+  const x = Number(split[0]);
+  const y = Number(split[1]);
+  const meta = {
+    hash,
+    x,
+    y,
+  };
+  if (!hashCache[x]) {
+    hashCache[x] = {};
+  }
+  hashCache[x][y] = meta;
+  mapOfHashes[meta.hash] = meta;
+  return [x, y];
 }
