e1d18681f46cf7be0f214690c270d2df30b828c3
Andrew Brower <monofuel@japura.net>
2021-01-16 17:58:55 -0800

    add dpad element

--- src/elements/custom/dpad/index.ts
+++ src/elements/custom/dpad/index.ts
@@ -2,10 +2,37 @@ import templateStr from "./index.html";
 import './index.scss';
 import { CustomElement } from "../CustomElement";
 
+enum DPAD_DIR {
+  'u' = 'u',
+  'd' = 'd',
+  'l' = 'l',
+  'r' = 'r'
+}
+const selectorMap: Record<DPAD_DIR, string> = {
+  'u': '#up',
+  'd': '#down',
+  'l': '#left',
+  'r': '#right'
+}
 export class DPadElement extends CustomElement {
   
   constructor() {
     super(templateStr);
     console.log("DPAD loaded");
+
+    // attach default handlers
+    for (const d in DPAD_DIR) {
+      this.setHandler(d, () => {
+        console.log(`dpad dir ${d}`)
+      })
+    }
+  }
+
+  setHandler(d: DPAD_DIR, handler: (ev: MouseEvent) => any) {
+    const selector = selectorMap[d]
+
+    const e = this.querySelector(selector)! as HTMLElement;
+    e.onmouseup = handler;
+
   }
 }
